6134f1369e90277019f4138f21ab7b2afe1366bf
Everton santos <everton0102@gmail.com>
2022-01-09 14:36:43 -0300

    logica do numero aleatorio

--- pages/api/produtos.js
+++ pages/api/produtos.js
@@ -1,12 +1,14 @@
-
+function numeroAleatorio(min = 1, max = 100000){
+    return parseInt(Math.random() * (max - min)) + min
+}
 
 
 
 export default function handler(req, res){
     res.status(200).json([
-        {id: 1, nome: 'Caneta', preco: 5.60},
-        {id: 1, nome: 'Caderno', preco: 20.60},
-        {id: 1, nome: 'Grafite', preco: 7.60},
-        {id: 1, nome: 'Borracha', preco: 4.40},
+        {id: numeroAleatorio(), nome: 'Caneta', preco: 5.60},
+        {id: numeroAleatorio(), nome: 'Caderno', preco: 20.60},
+        {id: numeroAleatorio(), nome: 'Grafite', preco: 7.60},
+        {id: numeroAleatorio(), nome: 'Borracha', preco: 4.40},
     ])
 }
