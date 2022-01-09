056c2a82ccb8fedc7919af3f6b91f60dda1171b1
Everton santos <everton0102@gmail.com>
2022-01-09 14:54:19 -0300

    redenrização dinamica ok

--- pages/api/produtos.js
+++ pages/api/produtos.js
@@ -2,8 +2,6 @@ function numeroAleatorio(min = 1, max = 100000){
     return parseInt(Math.random() * (max - min)) + min
 }
 
-
-
 export default function handler(req, res){
     res.status(200).json([
         {id: numeroAleatorio(), nome: 'Caneta', preco: 5.60},
